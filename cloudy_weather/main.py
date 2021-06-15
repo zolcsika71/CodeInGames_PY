33ba7c06c9a81dda0e8af42898cb649516e94c1c
zolcsika71 <bushdoctor2008@gmail.com>
2021-06-15 17:07:12 +0200

    start

--- cloudy_weather/main.py
+++ cloudy_weather/main.py
@@ -1,16 +1,74 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import sys
+import math
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Write an answer using print
+# To debug: print(f'Debug messages...', file=sys.stderr, flush=True)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Graph:
+    def __init__(self):
+        self.start = None
+        self.destination = None
+        self.current = None
+        self.nodes = {}
+        self.path = []
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    @staticmethod
+    def coord_to_id(x_, y_):
+        return str(x_) + ' ' + str(y_)
+
+    @staticmethod
+    def id_to_coord(object_id):
+        return [int(i) for i in object_id.split()]
+
+    def run(self):
+        self.get_input()
+
+    def get_input(self):
+        xs, ys = [int(i) for i in input().split()]
+
+        self.add_node(xs, ys, 0, False)
+
+        xd, yd = [int(i) for i in input().split()]
+
+        self.add_node(xd, yd, math.inf, False)
+
+        n = int(input())
+
+        for i in range(n):
+            xi, yi, wi, hi = [int(j) for j in input().split()]
+            for x in range(xi, xi + wi):
+                for y in range(yi, yi + hi):
+                    self.add_node(x, y, math.inf, True)
+
+        self.start = self.current = self.coord_to_id(xs, ys)
+        self.destination = self.coord_to_id(xd, yd)
+
+    def add_node(self, x_, y_, distance, cloudy, visited=False):
+        object_id = self.coord_to_id(x_, y_)
+        self.nodes[object_id] = {
+            'distance': distance,
+            'cloudy': cloudy,
+            'visited': visited
+        }
+
+    def get_neighbours(self):
+        x, y = self.id_to_coord(self.current)
+
+        left = self.coord_to_id(x - 1, y)
+        right = self.coord_to_id(x + 1, y)
+        top = self.coord_to_id(x, y - 1)
+        down = self.coord_to_id(x, y + 1)
+
+        if self.nodes[left] is None:
+            self.add_node(x - 1, y, 1, False)
+        if self.nodes[right] is None:
+            self.add_node(x + 1, y, 1, False)
+        if self.nodes[top] is None:
+            self.add_node(x, y - 1, 1, False)
+        if self.nodes[down] is None:
+            self.add_node(x, y + 1, False)
+
+
+graph = Graph()
